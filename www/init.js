2751228312d353de092918bd9e3e163e22ae6bb2
Norris <niw2@cdc.gov>
2024-08-02 12:30:54 -0400

    First commit

---
+++ www/init.js
@@ -0,0 +1,70 @@
+var isPivot = false; var initCols = [
+                      { title: "Year", className: "sub", "visible": false},
+                      { title: "row_var", className: "sub", "visible": false},
+                      { title: "rowLevels" , className: "main"},
+                      { title: "rowLevNum" , className: "sub", "visible": false},
+                      { title: "selected",   className: "sub", "visible" : false},
+                      
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay}]; var initLevels = []; var subLevels = ["Physician office visits","Non-physician office visits","Physician hosp. visits","Non-physician hosp. visits"];
